267af2220c8f8dd6a02e53c5c06210a4cbc3e61a
MXVX <kingdc718@gmail.com>
2022-12-17 04:52:33 +0700

    Add files via upload

---
+++ v2/menu/update.sh
@@ -0,0 +1,22 @@
+###########- COLOR CODE -##############
+echo -e " [INFO] Downloading Update File"
+sleep 1
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/menu.sh" && chmod +x /usr/bin/menu
+wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/menu-ss.sh" && chmod +x /usr/bin/menu-ss
+wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
+wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/menu-vless.sh" && chmod +x /usr/bin/menu-vless
+wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
+wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
+wget -q -O /usr/bin/menu-set "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/menu-set.sh" && chmod +x /usr/bin/menu-set
+wget -q -O /usr/bin/menu-theme "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/menu-theme.sh" && chmod +x /usr/bin/menu-theme
+wget -q -O /usr/bin/autoboot "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/autoboot.sh" && chmod +x /usr/bin/autoboot
+wget -q -O /usr/bin/menu-tcp "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/menu-tcp.sh" && chmod +x /usr/bin/menu-tcp
+wget -q -O /usr/bin/rebootvps "https://raw.githubusercontent.com/adammoi/xoc/main/corn/rebootvps.sh" && chmod +x /usr/bin/rebootvps
+wget -q -O /usr/bin/menu-dns "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/menu-dns.sh" && chmod +x /usr/bin/menu-dns
+wget -q -O /usr/bin/info "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/info.sh" && chmod +x /usr/bin/info
+wget -q -O /usr/bin/mspeed "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/menu-speedtest.sh" && chmod +x /usr/bin/mspeed
+wget -q -O /usr/bin/mbandwith "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/menu-bandwith.sh" && chmod +x /usr/bin/mbandwith
+wget -q -O /usr/bin/logcleaner "https://raw.githubusercontent.com/MXVXID/vx/main/v2/menu/logcleaner.sh" && chmod +x /usr/bin/logcleaner
+echo -e " [INFO] Update Successfully"
+sleep 1
+exit
